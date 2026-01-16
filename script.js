aff19c02f9799876e4d927982cf875112d791b70
M.Bilal Waris <bilalwaris543@gmail.com>
2026-01-16 23:24:26 +0500

    Added Functionality in Portfolio Section

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const resumeLists = document.querySelectorAll('.resume-list');
 const resumeBoxs = document.querySelectorAll('.resume-box');
 
 const portfolioLists = document.querySelectorAll('.portfolio-list');
+const portfolioBoxs = document.querySelectorAll('.portfolio-box');
 
 // resume section when clicking tab list
 resumeLists.forEach((list, idx) => {
     list.addEventListener('click', () => {
         document.querySelector('.resume-list.active').classList.remove('active');
         list.classList.add('active');
+
         document.querySelector('.resume-box.active').classList.remove('active');
         resumeBoxs[idx].classList.add('active');
     })
@@ -18,5 +20,8 @@ portfolioLists.forEach((list, idx) => {
     list.addEventListener('click', () => {
         document.querySelector('.portfolio-list.active').classList.remove('active');
         list.classList.add('active');
+
+        document.querySelector('.portfolio-box.active').classList.remove('active');
+        portfolioBoxs[idx].classList.add('active');
     })
 });
